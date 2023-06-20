b8ab0a5f16f2ab8c7494ad4ea59247ed3abc0412
teksts <mark.egelhoff1@gmail.com>
2023-06-20 12:41:44 -0700

    Add basic styling to compose tweet element

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,5 @@ $(document).ready(function() {
   $("textarea").on("input", function(event) {
     console.log($(this).siblings("div")
                         .children(".counter").val(140 - $(this).val().length));
-    // = 140 - $(this).val().length;
-    // console.log(140 - $(this).val().length);
   });
 });
