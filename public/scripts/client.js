8946c8cc9b453e9e750a01fb80474898ce6de867
teksts <mark.egelhoff1@gmail.com>
2023-06-20 20:51:32 -0700

    Adding untracked files

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const temporaryTestData =  [
+  const temporaryTestData = [
     {
       "user": {
         "name": "Newton",
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
   $("textarea").on("input", function(event) {
-    $(this).siblings("div")
-            .children(".counter").val(140 - $(this).val().length);
+    const counter = $(this).siblings("div").children(".counter");
+    const defaultColor = $("body").css("color");
+    counter.val(140 - $(this).val().length);
+    if (counter.val() < 0) {
+      counter.css("color", "red");
+      console.log("set to red", counter.css("color"));
+    }
+    if (counter.css("color") === "rgb(255, 0, 0)" && counter.val() > 0) {
+      console.log(defaultColor);
+      counter.css("color", "");
+    }
   });
 });
