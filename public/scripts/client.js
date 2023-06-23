eed80831d068957e110c7a8473f9c4a8271db535
teksts <mark.egelhoff1@gmail.com>
2023-06-22 22:17:13 -0700

    Fix bug causing duplicate tweet rendering in feed
    Add comment documentation throughout codebase

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,14 +1,17 @@
+// Wrap all client-side code in a $(document).ready() callback to ensure correct behaviour
 $(document).ready(function() {
-  
+
+  // Correctly formats timestamps on tweets to show time since tweet created in a readable format
   const getTimePassed = function (timestamp) {
     return timeago.format(timestamp);
   };
 
+  // Builds tweet elements out of tweet objects
   const createTweetElement = function(tweet) {
-    // Build tweeter info in header
     const $tweeterInfo = $(`<div class="tweeter-info"><div><img src="${tweet["user"]["avatars"]}"></img></div><div class="tweeter-name">${tweet["user"]["name"]}</div></div>`).prop("outerHTML");
     const $handle = $(`<div class="handle">${tweet["user"]["handle"]}</div>`).prop("outerHTML");
     const $header = $(`<header>${$tweeterInfo}${$handle}</header>`).prop("outerHTML");
+    // utilise .text when constructing tweet body to prevent XSS vulnerabilities
     const $body = $(`<div class="tweet-body"></div>`).text(tweet["content"]["text"]).prop("outerHTML");
     const $timestamp = $(`<div class="date">${getTimePassed(tweet["created_at"])}</div>`).prop("outerHTML");
     const $actions = $(`<div class="tweet-action-cluster"><div class="tweet-action"><i class="fa-solid fa-flag"></i></div><div class="tweet-action"><i class="fa-solid fa-retweet"></i></div><div class="tweet-action"><i class="fa-solid fa-heart"></i></div></div>`).prop("outerHTML");
@@ -17,6 +20,7 @@ $(document).ready(function() {
     return $tweet;
   };
 
+  // Builds the error banner
   const createErrorElement = function() {
     const $errorSymbol = $(`<div class="error-symbol"><i class="fa-solid fa-skull-crossbones fa-xl"></i></div>`).prop("outerHTML");
     const $errorTitle = $(`<div class="error-msg-title">HOLD ON SAILOR!</div>`).prop("outerHTML");
@@ -26,6 +30,7 @@ $(document).ready(function() {
     $("main").prepend($errorPopup);
   }
 
+  // Populates error banner with appropriate error message and insert into the DOM at the appropriate location
   const renderError = function(msg) {
     $errorPopup = $(".invalid-tweet-error");
     $errorBody = $(".error-msg-body");
@@ -35,14 +40,24 @@ $(document).ready(function() {
     });
   };
 
+  // Inserts tweet feed into DOM
   const renderTweets = function (tweetDB) {
-    // $("main").css("margin-top", "180px");
-    for (const tweet of tweetDB.reverse()) {
-      const $tweet = createTweetElement(tweet);
-      $('#tweets-container').append($tweet);
+    // if tweet feed already loaded, only render new tweet
+    if ($("#tweets-container").data("loaded")) {
+      const newTweet = tweetDB.reverse()[0];
+      const $newTweet = createTweetElement(newTweet);
+      $('#tweets-container').prepend($newTweet);
+    // if tweet feed unloaded, render all tweets
+    } else {
+      for (const tweet of tweetDB.reverse()) {
+        const $tweet = createTweetElement(tweet);
+        $('#tweets-container').append($tweet);
+        $("#tweets-container").data("loaded", true);
+      }
     }
   };
-
+  
+  // Feeds tweet database into renderTweets() using AJAX to avoid page reloads
   const loadTweets = function () {
     $.ajax({
       method: "GET",
@@ -53,18 +68,19 @@ $(document).ready(function() {
     })
   };
 
+  // Insert hidden, empty error banner
   createErrorElement();
 
-  $(".new-tweet").data("clicked", false);
-
-  // set flag for initial section state
+  // set flag for initial composer display state
   $(".new-tweet").data("collapsed", true);
 
-  // on-click handler for new-tweet expansion
+  // set flag marking if tweet feed has been loaded
+  $("#tweets-container").data("loaded", false);
+
+  // on-click handler for new tweet composer expansion
   $(".nav-right-side").on("click", function () {
     // initialize target DOM elements
     const $newTweetSection = $(this).parents("body").children("main").children(".new-tweet");
-    console.log($($newTweetSection).children().children("textarea"));
     const $newTweetTextArea = $($newTweetSection).children().children("textarea");
     // if collapsed, expanded and adjust flag
     if ($newTweetSection.data("collapsed")) {
@@ -81,7 +97,9 @@ $(document).ready(function() {
     }
   });
 
+  // on-submit handler for new tweet creation
   $("form").on("submit", function (event) {
+    // bloc default behaviour to ensure data is only posted if input has been validated
     event.preventDefault();
     const data = $(this).serialize();
     const formContent = $(this).children("#tweet-text").val();
@@ -91,13 +109,17 @@ $(document).ready(function() {
     const $errorPopup = $(this).parents("main").children(".invalid-tweet-error")
     const emptyTweetMsg = "You forgot to write something!";
     const overLimitMsg = "Too wordy! Tweets have to be 140 characters or less.";
+    // if error is showing, slide up banner (to be slid down again later pending validation)
     if ($errorPopup.css("display") !== "none") {
       $errorPopup.slideUp();
     }
+    // block submission and display error if tweet empty
     if (!formContent) {
       renderError(emptyTweetMsg);
+    // block submission and display error if tweet too large
     } else if (overLimit) {
       renderError(overLimitMsg);
+    // tweet is valid, post to server and update tweet feed
     } else {
       $.ajax({
         method: "POST",
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,17 @@
+// Wrap in $(document).ready() to ensure code only executes when DOM structure is ready to be traversed
 $(document).ready(function() {
+  // on-input handler for compose new tweet text field
   $("textarea").on("input", function(event) {
     const counter = $(this).siblings("div").children(".counter");
     const defaultColor = $("body").css("color");
+    // update value of the counter upon each input to reflect tweet length
     counter.val(140 - $(this).val().length);
+    // adjust counter color if limit exceeded
     if (counter.val() < 0) {
       counter.css("color", "red");
       counter.data("overLimit", true);
     }
+    // reset counter color of brought back under limit
     if (counter.data("overLimit") === true && counter.val() > 0) {
       counter.css("color", defaultColor);
       counter.data("overLimit", false);
